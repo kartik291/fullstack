45b5f39c06f20fee0442ce1f56182f9fcf1ff75a
kartik291 <119069714+kartik291@users.noreply.github.com>
2024-03-11 01:27:10 +0530

    Add files via upload

---
+++ lec1.html/home.js
@@ -0,0 +1,72 @@
+console.log("hello geeks");
+console.log("my name is kartik chauhan")
+// log means to write this text in the console
+// console is an api provided by an browser
+console.log(7+3);
+// variables-that can hold some data or information
+var name;
+name="hello geeksforgeeks"
+console.log(name);
+console.log("hello,"+name);
+name="my name is prakash sarkari";
+console.log(name)
+
+let text;
+text="javascript is the best in the world";
+console.log(text);
+// const must be intialized 
+const that= "hello";
+console.log(that);
+// that="ho";
+console.log(that);
+
+// let userName;
+let age;
+let _;
+let $="hello";
+console.log($);
+let number1;
+let number_2;
+let userName="prakash";
+console.log("my name is "+userName);
+// string
+let username="prakash";
+console.log(typeof username);
+let go=56;
+console.log(go+" years old");
+console.log(typeof go);
+// boolean
+let isPorductInCart= true;
+if (isPorductInCart==false){
+    console.log("product not added in the cart");
+
+}
+else{
+    console.log("product in the cart");
+}
+console.log(typeof isPorductInCart)
+let user;
+console.log(user);
+
+
+// object
+const personDetails={
+    name:"prakash",
+    age:99,
+    education:"i am an engineer"
+}
+console.log(personDetails);
+console.log(typeof personDetails);
+const arrOfNumbers=[1,2,3,4,5];
+console.log(typeof arrOfNumbers);
+
+let noValue=null;
+console.log( noValue);
+let Age=99;
+let message="my name is "+userName+" and i am "+Age+"  years old";
+console.log(message);
+
+// template literals/strings
+
+let Text=`my name is ${userName}`;
+console.log(Text)
